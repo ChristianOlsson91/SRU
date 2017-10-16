5fe2ec22a6972dd53a2f0079ef70ef7724204fc9
ChristianOlsson91 <Christian__91_@hotmail.com>
2017-10-16 10:45:44 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package se.hig.CN.control;
+
+import se.hig.CN.model.FileCreator;
+import se.hig.CN.view.GUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new GUI();
+		
+		FileCreator c = new FileCreator();
+		c.createStructureForFile();
+	}
+}
