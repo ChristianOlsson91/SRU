0a29121d8fbf77fbb092653bd529c77e51ae95cf
ChristianOlsson91 <Christian__91_@hotmail.com>
2017-11-16 09:57:39 +0100

    Update TextFieldController.java

--- src/se.hig.cn.control/TextFieldController.java
+++ src/se.hig.cn.control/TextFieldController.java
@@ -2,17 +2,33 @@ package se.hig.cn.control;
 
 import java.util.ArrayList;
 
+import javax.swing.JTextField;
+
+/**
+ * Kollar att alla textfält är korrekta.
+ * 
+ * @author Christian Olsson, Nicolas Suau Carvajal
+ * @version 2017-11-14
+ *
+ */
 public class TextFieldController {
-	ArrayList<String> wrongList = new ArrayList<>();
+	ArrayList<JTextField> wrongList = new ArrayList<>();
 
-	public ArrayList<String> checkTextFields(ArrayList<String> textList) {
-
-		for (int i = 0; i < textList.size(); i++)
-			if (!textList.get(i).matches("[0-9]+"))
-				wrongList.add(textList.get(i));
-
+	/**
+	 * Tar emot en lista med alla textfält från GUI-klassen och kollar igenom dem och skickar sedan tillbaka textfälten som är inkorrrekta.
+	 * 
+	 * @param textfieldList - Lista med alla textfält från GUI-klassen.
+	 * @return wrongList - Lista med alla textfält som är inkorrekta.
+	 */
+	public ArrayList<JTextField> checkTextFields(ArrayList<JTextField> textfieldList) {
+		
+		for(int i = 0; i < textfieldList.size(); i++) {
+			if(textfieldList.get(i).getText().length() == 0)
+				wrongList.add(textfieldList.get(i));
+		}
+		
 		for (int i = 0; i < wrongList.size(); i++) {
-			System.out.println(wrongList.get(i));
+			System.out.println(i + ". " + wrongList.get(i).getText());
 		}
 
 		return wrongList;
