ab96211348dd6df8a077786f78838f2305aecd4f
ChristianOlsson91 <Christian__91_@hotmail.com>
2017-11-20 13:20:26 +0100

    Update Main.java

--- src/se.hig.cn.control/Main.java
+++ src/se.hig.cn.control/Main.java
@@ -1,14 +1,16 @@
 package se.hig.cn.control;
 
-import se.hig.CN.model.FileCreator;
-import se.hig.CN.view.GUI;
+import se.hig.cn.view.GUI;
 
+/**
+ * Skapar en ny instans av GUI-klassen.
+ * 
+ * @author Christian Olsson, Nicolas Suau Carvajal
+ * @version 2017-11-14
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		new GUI();
-		
-		FileCreator c = new FileCreator();
-		c.createStructureForFile();
 	}
 }
